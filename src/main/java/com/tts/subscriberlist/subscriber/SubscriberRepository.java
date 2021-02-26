76ecf7c82850f3ab1978b7575201b04eb7242c0f
AdrienneY29 <55600245+AdrienneY29@users.noreply.github.com>
2021-02-26 02:33:48 -0600

    first commit

---
+++ src/main/java/com/tts/subscriberlist/subscriber/SubscriberRepository.java
@@ -0,0 +1,9 @@
+package com.tts.subscriberlist.subscriber;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface SubscriberRepository extends CrudRepository<Subscriber, Long> {
+
+}
+
+
